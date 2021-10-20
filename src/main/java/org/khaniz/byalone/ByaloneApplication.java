14d95a7741081329f2a24de33b5fb417a56fc7c6
parkkihoon <gjf.kee@gmail.com>
2021-10-20 21:38:02 +0900

    Initial commit

---
+++ src/main/java/org/khaniz/byalone/ByaloneApplication.java
@@ -0,0 +1,13 @@
+package org.khaniz.byalone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ByaloneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ByaloneApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/khaniz/byalone/ByaloneApplicationTests.java
@@ -0,0 +1,13 @@
+package org.khaniz.byalone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ByaloneApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
